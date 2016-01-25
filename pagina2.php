60e927901cb67fd6207f0b9aec9206899aa827ac
bushindo <alromo7@gmail.com>
2016-01-25 19:15:58 +0100

    Primeros pasos

---
+++ pagina2.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Captura de datos del formulario</title>
+    </head>
+    <body>
+        <?php
+        echo "La persona ";
+        echo $_REQUEST['nombre'];
+        if($_REQUEST['edad']>18){
+           echo " es mayor de edad";
+        }
+           else{
+            echo " no es mayor de edad";
+        };
+        ?>
+    </body>    
+</html>
+
+
